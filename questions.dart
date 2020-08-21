54b76f0f9240151408536f693faa2e59f3517473
Osamakhandart <62379483+Osamakhandart@users.noreply.github.com>
2020-08-21 20:43:18 +0500

    Add files via upload

---
+++ questions.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Question {
+  String question;
+  bool answer;
+  Question(String ques, bool ans) {
+    question = ques;
+    answer = ans;
+  }
+}
